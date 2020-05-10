94a2700d31a7c08bd8b9aff4593f95bd96c5414f
Cristihan Albarran <cristihanjm@gmail.com>
2020-05-10 01:09:52 -0500

    feature] coonect reach-router to handle routes and implementing lazy load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Router } from '@reach/router';
+import Layout from './components/Layout/Layout';
+import History from './pages/History';
 
-const App = () => <h1>App</h1>;
+const Home = lazy(() => import('./pages/Home'));
+const Users = lazy(() => import('./pages/Users'));
+
+const App = () => (
+  <Layout>
+    <Suspense fallback={<h1>Loaing..</h1>}>
+      <Router>
+        <Home path="/" />
+        <Users path="/users" />
+        <History path="/history/:id" />
+      </Router>
+    </Suspense>
+  </Layout>
+);
 
 export default App;
