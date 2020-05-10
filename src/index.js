bb0a739e0738b9eeef563bb7df2539d10782be4d
Cristihan Albarran <cristihanjm@gmail.com>
2020-05-10 01:10:27 -0500

    adding packgas to indexhtml

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import 'regenerator-runtime/runtime';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
+import store from './store';
 
 render(
-  <>
+  <Provider store={store}>
     <GlobalStyles />
     <App />
-  </>,
+  </Provider>,
   document.querySelector('#app')
 );
